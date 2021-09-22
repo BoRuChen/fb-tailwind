b3d5c2cafc53ca4fc36d59b3ff2e223a78e94cae
Boru-Chen <56582854+Boru-Chen@users.noreply.github.com>
2021-09-23 01:23:55 +0800

    left item size

--- src/components/leftItem/index.js
+++ src/components/leftItem/index.js
@@ -1,8 +1,8 @@
 const LeftItem = (props) => {
 
-    return (<div className='flex w-full mt-2 p-2 items-center hover:bg-fb-popover cursor-pointer'>
+    return (<div className='flex w-full mt-2 p-1 items-center hover:bg-fb-popover cursor-pointer'>
         <img src={props.url} alt={props.name}
-             className={`h-[40px] w-[40px] ${props.ture && 'rounded-full'}`}/>
+             className={`h-[30px] w-[30px] ${props.ture && 'rounded-full'}`}/>
         <div className='flex-1 ml-2'>
             <p className='text-white text-xl'>{props.name}</p>
         </div>
